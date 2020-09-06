bc88fa90a4156adf921e53c034329ba7196e28a2
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-09-06 16:44:16 +0000

    auto-start all available repos

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const express = require('express')
 const fetch = require('node-fetch');
 const app = express()
 
+const repo_update = (repo) => {
+  const repo_path = __dirname + path.sep + ".." + path.sep + repo
+  shell.cd(repo_path)
+  shell.exec(`git pull`)
+  const { setup, run } = JSON.parse(fs.readFileSync(path.resolve(repo_path, '.run.json')));
+  shell.exec(setup)
+  const systemd_service =
+    `[Unit]\n` +
+    `Description=${repo} Service\n` +
+    `[Service]\n` +
+    `ExecStart=/usr/bin/sh -c "cd ${repo_path} && PORT=${current_port} ${run}"\n`
+  fs.writeFileSync(`/etc/systemd/system/${repo}.service`, systemd_service)
+  active_services[repo] = current_port
+  shell.exec(`sudo systemctl daemon-reload && sudo systemctl restart ${repo}`)
+  if (++current_port > 4000) current_port = 3000
+}
+
+
 const repo_owner = "Unn4m3DD"
 let global_ip;
 fetch("https://ipecho.net/plain").then(async (res) => {
@@ -15,29 +33,17 @@ fetch("https://ipecho.net/plain").then(async (res) => {
 let active_services = {}
 
 let current_port = 3000
-
-shell.cd(__dirname + path.sep + "..")
+fs.readdirSync(__dirname + path.sep + "..").filter(e => e != "post_web_service").forEach(e => repo_update(e))
 
 app.get('/add', (req, res) => {
+  shell.cd(__dirname + path.sep + "..")
   shell.exec(`git clone https://github.com/${repo_owner}/${req.query.repo}`)
   res.send(`https://github.com/${repo_owner}/${req.query.repo} cloned`)
 })
+
 app.get('/update', (req, res) => {
-  const repo_path = __dirname + path.sep + ".." + path.sep + req.query.repo
-  shell.cd(repo_path)
-  shell.exec(`git pull`)
-  const { setup, run } = JSON.parse(fs.readFileSync(path.resolve(repo_path, '.run.json')));
-  shell.exec(setup)
-  const systemd_service =
-    `[Unit]\n` +
-    `Description=${req.query.repo} Service\n` +
-    `[Service]\n` +
-    `ExecStart=/usr/bin/sh -c "cd ${repo_path} && PORT=${current_port} ${run}"\n`
-  fs.writeFileSync(`/etc/systemd/system/${req.query.repo}.service`, systemd_service)
-  active_services[req.query.repo] = current_port
-  shell.exec(`sudo systemctl daemon-reload && sudo systemctl restart ${req.query.repo}`)
-  res.send(`<a href="http://${global_ip}:${current_port}">${req.query.repo} updated</a>`)
-  if (++current_port > 4000) current_port = 3000
+  repo_update(req.query.repo)
+  res.send(`<a href="http://${global_ip}:${current_port - 1}">${req.query.repo} updated</a>`)
 })
 app.get('/del', (req, res) => {
   const repo_path = __dirname + path.sep + ".." + path.sep + req.query.repo
@@ -48,7 +54,7 @@ app.get('/del', (req, res) => {
 app.get('/', (req, res) => {
   let result = ""
   for (let key in active_services) {
-    result += `<a href="http://${global_ip}:${active_services[key]}">${key}</a>`
+    result += `<a href="http://${global_ip}:${active_services[key]}">${key}</a></br>`
   }
   res.send(result)
 })
