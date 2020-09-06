6f5fdcdd9ed445bac1baa07ef405bc14c57cc1bb
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-09-06 15:59:33 +0000

    delete and update updated

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const shell = require('shelljs')
 const express = require('express')
+const fetch = require('node-fetch');
 const app = express()
 
 const repo_owner = "Unn4m3DD"
 let global_ip;
-externalip((err, ip) => {
-  global_ip = ip
+fetch("https://ipecho.net/plain").then(async (res) => {
+  global_ip = await res.text()
   console.log(`http://${global_ip}`)
 })
 
@@ -15,15 +16,13 @@ let active_services = {}
 
 let current_port = 3000
 
-
-
 shell.cd(__dirname + path.sep + "..")
 
-app.get('/clone', (req, res) => {
+app.get('/add', (req, res) => {
   shell.exec(`git clone https://github.com/${repo_owner}/${req.query.repo}`)
   res.send(`https://github.com/${repo_owner}/${req.query.repo} cloned`)
 })
-app.get('/pull', (req, res) => {
+app.get('/update', (req, res) => {
   const repo_path = __dirname + path.sep + ".." + path.sep + req.query.repo
   shell.cd(repo_path)
   shell.exec(`git pull`)
@@ -33,16 +32,20 @@ app.get('/pull', (req, res) => {
     `[Unit]\n` +
     `Description=${req.query.repo} Service\n` +
     `[Service]\n` +
-    `ExecStart=/usr/bin/sh -c "PORT=${current_port} && cd ${repo_path} && ${run}"\n`
+    `ExecStart=/usr/bin/sh -c "cd ${repo_path} && PORT=${current_port} ${run}"\n`
   fs.writeFileSync(`/etc/systemd/system/${req.query.repo}.service`, systemd_service)
-  active_services[req.query.repo] = current_port++
-  if (current_port > 4000) current_port = 3000
-  res.send(`${req.query.repo} up to date`)
-
+  active_services[req.query.repo] = current_port
+  shell.exec(`sudo systemctl daemon-reload && sudo systemctl restart ${req.query.repo}`)
+  res.send(`<a href="http://${global_ip}:${current_port}">${req.query.repo} updated</a>`)
+  if (++current_port > 4000) current_port = 3000
 })
 app.get('/del', (req, res) => {
+  const repo_path = __dirname + path.sep + ".." + path.sep + req.query.repo
+  shell.exec(`sudo systemctl stop ${req.query.repo}`)
+  shell.exec(`sudo rm -rf ${repo_path}`)
+  res.send(`${deleted} up to date`)
 })
-app.get('/list', (req, res) => {
+app.get('/', (req, res) => {
   let result = ""
   for (let key in active_services) {
     result += `<a href="http://${global_ip}:${active_services[key]}">${key}</a>`
