4bcc6bdeb794ea1c6f537009f8691ef66245b703
Andre Patacas <guerreiroapatacas@gmail.com>
2020-09-06 16:25:45 +0100

    list services added

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const express = require('express')
 const app = express()
 
 const repo_owner = "Unn4m3DD"
+let global_ip;
+externalip((err, ip) => {
+  global_ip = ip
+  console.log(`http://${global_ip}`)
+})
+
+let active_services = {}
+
+let current_port = 3000
+
+
 
 shell.cd(__dirname + path.sep + "..")
 
@@ -19,21 +30,24 @@ app.get('/pull', (req, res) => {
   const { setup, run } = JSON.parse(fs.readFileSync(path.resolve(repo_path, '.run.json')));
   shell.exec(setup)
   const systemd_service =
-    `[Unit]
-  Description=${req.query.repo} Service
-  
-  [Service]
-  ExecStart=/usr/bin/sh -c "cd ${repo_path} && ${run}"`
+    `[Unit]\n` +
+    `Description=${req.query.repo} Service\n` +
+    `[Service]\n` +
+    `ExecStart=/usr/bin/sh -c "PORT=${current_port} && cd ${repo_path} && ${run}"\n`
   fs.writeFileSync(`/etc/systemd/system/${req.query.repo}.service`, systemd_service)
-
+  active_services[req.query.repo] = current_port++
+  if (current_port > 4000) current_port = 3000
   res.send(`${req.query.repo} up to date`)
 
 })
 app.get('/del', (req, res) => {
 })
-
-app.listen(
-  3000, () => {
-    console.log("http://localhost:3000")
+app.get('/list', (req, res) => {
+  let result = ""
+  for (let key in active_services) {
+    result += `<a href="http://${global_ip}:${active_services[key]}">${key}</a>`
   }
-)
+  res.send(result)
+})
+
+app.listen(80)
