2748d19faa7f61c03154b2817afc0a3a2dbd6cc2
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-01-26 20:04:17 +0000

    restart logic updated

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const express = require('express')
 const fetch = require('node-fetch');
 const app = express()
 
-const repo_update = (repo) => {
+const repo_update = async (repo) => {
   const repo_path = __dirname + path.sep + ".." + path.sep + repo
   shell.cd(repo_path)
-  shell.exec(`git pull`)
+  const git_status = shell.exec(`git pull --no-rebase`).stdout
   const { setup, run } = JSON.parse(fs.readFileSync(path.resolve(repo_path, '.run.json')));
-  shell.exec(setup)
+  console.log(git_status)
+  if (git_status.indexOf("up to date") == -1 && setup != "")
+    shell.exec(setup)
   const systemd_service =
     `[Unit]\n` +
     `Description=${repo} Service\n` +
@@ -19,8 +21,8 @@ const repo_update = (repo) => {
   fs.writeFileSync(`/etc/systemd/system/${repo}.service`, systemd_service)
   active_services[repo] = current_port
   shell.exec(`sudo systemctl daemon-reload && sudo systemctl restart ${repo}`)
-  
-  if(++current_port > 4000) current_port = 3000
+
+  if (++current_port > 2000) current_port = 1000
 }
 
 
@@ -28,13 +30,13 @@ const repo_owner = "Unn4m3DD"
 let global_ip = "148.63.171.198";
 fetch("https://ipecho.net/plain").then(async (res) => {
   global_ip = await res.text()
-  if(global_ip == undefined) global_ip = "148.63.171.198";
+  if (global_ip == undefined) global_ip = "148.63.171.198";
   console.log(`http://${global_ip}`)
 })
 
 let active_services = {}
 
-let current_port = 3000
+let current_port = 1000
 fs.readdirSync(__dirname + path.sep + "..").filter(e => e != "post_web_service").forEach(e => repo_update(e))
 app.get('/add', (req, res) => {
   shell.cd(__dirname + path.sep + "..")
@@ -42,8 +44,8 @@ app.get('/add', (req, res) => {
   res.send(`https://github.com/${repo_owner}/${req.query.repo} cloned`)
 })
 
-app.get('/update', (req, res) => {
-  repo_update(req.query.repo)
+app.get('/update', async (req, res) => {
+  await repo_update(req.query.repo)
   res.send(`<a href="http://${global_ip}:${current_port - 1}">${req.query.repo} updated</a>`)
 })
 app.get('/del', (req, res) => {
